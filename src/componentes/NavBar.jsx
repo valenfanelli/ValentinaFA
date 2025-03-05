0c917e46eeeaeb0849f623f081f05b6f601eea18
valenfanelli <valentinafanelliaddiechi@hotmail.com>
2025-03-05 12:54:16 -0300

    Subo cambios en navbar responsive

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -2,16 +2,9 @@ import { Link } from 'react-router-dom';
 import React, { useState } from "react";
 import '../App.css';
 export const NavBar = () => {
-    const [manu, setMenu] = useState(false);
-    const toggleMenu = () => {
-        setMenu(!menu);
-    };
     return <>
         <nav className="navbar navbar-expand-lg bg-nav navnav roboto-mono">
-            <div className="menu-icon navbar-nav mnavbar" onClick={toggleMenu}>
-                <i className="fas fa-bars"></i>
-            </div>
-            <div className="navbar-nav mnavbar mx-auto m-3 fs-6">
+            <div className="navbar-nav text-center mx-auto m-3 fs-6">
                 <Link className="nav-item nav-link text-white" to="/">HOME</Link>
                 <Link className="nav-item nav-link text-white" to="/perfil">PERFIL</Link>
                 <Link className="nav-item nav-link text-white" to="/educacion">EDUCACIÓN</Link>
